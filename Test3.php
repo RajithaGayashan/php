c3e573a7f8fcee0fa420b4f6aac25cd957228d1a
RajithaGayashan <rajithagayashan95@gmail.com>
2019-09-24 20:23:15 +0530

    Variables

---
+++ Test3.php
@@ -0,0 +1,11 @@
+<?php
+ $f_text="My name is ";
+ $l_text="Rajitha Gayashan";
+ $full_text=$f_text;
+ $full_text.=$l_text;
+ echo $full_text;
+ echo "<br>"
+ 
+?>
+All simple :<?php echo strtolower($full_text); ?><br>
+All Capital:<?php  echo strtoupper($full_text);?><br>
