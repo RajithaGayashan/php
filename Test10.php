b22a00dc179355d36356081c07cf4c3c87f888d2
Rajitha Gayashan <40441007+RajithaGayashan@users.noreply.github.com>
2019-10-17 07:55:28 +0530

    Add files via upload

---
+++ Test10.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>HHH</title>
+</head>
+<body>
+
+<form>
+<action="txt.php" method="post">
+<input type="text" name="textfield" id="">
+</form>
+
+</body>
+</html>
